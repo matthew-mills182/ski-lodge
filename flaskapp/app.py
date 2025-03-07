a9c8c93d6675238b438e23d8158cfa9d5eb8e8e0
millsmat <millsmat@iu.edu>
2025-03-07 15:10:58 +0000

    done in lab, started on routes and loading data from csv

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,11 +1,24 @@
 # Copyright © 2023-2025, Indiana University
 # BSD 3-Clause License
+import csv
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def load_people():
+    with open('people.csv', 'r') as csvf:
+        reader = csv.DictReader(csvf)
+    return list(reader)
 
 @app.route("/")
 def render_index():
     return render_template("index.html")
+
+@app.route("/people/")
+def render_people():
+    return render_template("people.html")
+
+@app.route("/equipment/")
+def render_equipment():
+    return render_template("equipment.html")

6a1a6f81be40967bc55aba590ef11cf5774c6a1d
millsmat <millsmat@iu.edu>
2025-04-01 22:34:23 +0000

    added add equipment function for tag 3.0

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,8 +1,10 @@
 # Copyright © 2023-2025, Indiana University
 # BSD 3-Clause License
+# playin
+
 import csv
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -17,7 +19,24 @@ def load_people_id():
         for row in csv.DictReader(csvf):
             people[row['Name']] = row
         return people
-     
+    
+def save_people(new_person):
+    
+    with open("people.csv", mode="a", newline="") as csv_file:
+        fieldnames = ["Name", "Email", "Height", "Shoe Size"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        
+        
+        writer.writerow(new_person)
+
+
+def save_equipment(new_equipment):
+    with open("misc.csv", mode="a", newline="\n") as csv_file:  
+        fieldnames = ["name", "quantity", "max_quantity"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writerow(new_equipment)
+
 
 def load_miscellaneous():
     with open("misc.csv", "r") as csvf:
@@ -62,3 +81,38 @@ def render_equipment():
     boots = load_boots()
     return render_template("equipment.html",miscellaneous=miscellaneous, poles=poles, boots = boots, skis = skis)
 
+
+
+@app.route("/add-person/", methods=["GET", "POST"])
+def handle_new_person():
+    if request.method == "GET":
+        return render_template("add-person.html")  
+    
+    
+    new_person = {
+        "Name": request.form["name"],
+        "Email": request.form["email"],
+        "Height": request.form["height"],
+        "Shoe Size": request.form["shoe_size"]
+    }
+    
+    
+    save_people(new_person)
+    
+    return redirect(url_for("render_people"))
+
+
+@app.route("/add-equipment/", methods=["GET", "POST"])
+def handle_new_equipment():
+    if request.method == "GET":
+        return render_template("add-equipment.html")
+    
+    new_equipment = {
+        "name": request.form["name"],  
+        "quantity": request.form["number"],  
+        "max_quantity": request.form["number"]  
+    }
+    
+    save_equipment(new_equipment)
+    
+    return redirect(url_for("render_equipment"))
